2abefccf47b5a34717228789bcf8e051e563d330
training <249100089@qq.com>
2019-07-11 13:25:22 +0800

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+
+@SpringBootApplication
+@EnableTask
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings)throws  Exception{
+		System.out.println("======>Hello,World!");
+	}
+
+}
